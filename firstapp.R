57b5c1d2ebf865997b70c223b4f114d9696c8666
Jasper <halvorsonjasper11@gmail.com>
2024-04-05 06:29:09 -0700

    Version Functions locally

--- firstapp.R
+++ firstapp.R
@@ -43,6 +43,8 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
 
 # Define server function
 server <- function(input, output) {
+  # Reactive value to store the path of the last created file
+  lastCreatedFile <- reactiveVal()
   
   # # Reactive function to read uploaded CSV file
   # data <- reactive({
@@ -51,7 +53,7 @@ server <- function(input, output) {
   # })
   
   # Reactive function to read uploaded Patient Data CSV file
-  all_data <- reactive({
+  all_data_raw <- reactive({
     req(input$patientData)  # Ensure file is uploaded
     read.csv(input$patientData$datapath, stringsAsFactors = FALSE)
   })
@@ -66,8 +68,8 @@ server <- function(input, output) {
   
   # Render data table for all_data
   output$table1 <- renderDT({
-    req(all_data())
-    all_data()
+    req(all_data_raw())
+    all_data_raw()
   })
   
   # Render data table for variables
@@ -83,14 +85,84 @@ server <- function(input, output) {
   # Create last set of graphs without any parameters highlighted (variables and data read in above)
   stimulants<-c("DMSO","Fluzone", "COVID_WT", "COVID_BA4_5")
   
+  Neg_to_Zero<-function(x){
+    ifelse((x<=0.005), 0.005, x)
+  }
+  
+  # boxcox function (I added a default lambda value of 0.5)
+  #default lambda value of 0.5
+  bc <- function(x,l=0.5){if(l==0) return(log(x)) else return((x^l-1)/l)}
+  # inverse boxcox function
+  ibc <- function(x,l=0.5){if(l==0) return(exp(x)) else return((x*l+1)^(1/l))}
+  
+  #var<-"CD4_CD134pCD25p"
+  
+  # Assuming all_data is defined as before, we create a new reactive expression for the filtered data
+  all_data_filtered <- reactive({
+    req(all_data_raw())  # Ensure all_data is available
+    data <- all_data_raw()  # Get the data frame
+    
+    # Now filter the data frame
+    data %>% 
+      filter(Stim != "Fluzone" & Stim != "Cytostim") %>%
+      filter(DonorID != "UBC_004" & DonorID != "UBC_059") %>%
+      filter(Timepoint != "V1" & Timepoint != "6M" & Timepoint != "1Y") %>%
+      arrange(Stim, Timepoint, DonorID)
+  })
   
   
+  # all_data <- all_data %>%
+  #   arrange(Stim, Timepoint, DonorID)
+  # all_data
   
   
-  
-  
-  
-  
+  observe({
+    req(input$patientData, input$AIMVariables) # Ensure both files are uploaded
+    allDataValue <- all_data_filtered() # Get the current value of all_data
+    variableNames <- variables()$variable # Assuming variables() returns a dataframe with a column 'variable'
+    
+    for (var in variableNames) {
+      
+      subtracted_data<-allDataValue %>% 
+        dplyr::select(DonorID:Stim,{{var}}) %>% 
+        tidyr::pivot_wider(names_from = Stim, values_from = {{var}}) %>% 
+        dplyr::mutate(Covid_WT_sum=COVID_WT-DMSO, Covid_BA4_5_sum=COVID_BA4_5-DMSO) %>% 
+        tidyr::pivot_longer(DMSO:Covid_BA4_5_sum, names_to="Stimulant", values_to = {{var}}) %>%
+        dplyr::mutate(across(DonorID:Stimulant,as.factor)) %>%
+        dplyr::filter(Stimulant != "COVID_WT" & Stimulant != "COVID_BA4_5") %>%
+        dplyr::mutate(Stimulant=fct_recode(Stimulant, COVID_WT="Covid_WT_sum", COVID_BA4_5="Covid_BA4_5_sum")) %>% 
+        dplyr::mutate(Timepoint=fct_relevel(Timepoint,"VY","V2","V3"))
+      
+      #use lapply to apply the Neg to Zero function to all rows
+      #subtracted_data[[var]] <- lapply(subtracted_data[[var]], Neg_to_Zero)
+      #subtracted_data[[var]] <- unlist(subtracted_data[[var]]) 
+      filePath <- glue::glue("PREVENT_Subtracted_{var}.csv")
+      subtracted_data <- arrange(subtracted_data, Stimulant, Timepoint, DonorID)
+      write.csv(subtracted_data, filePath)
+      lastCreatedFile(filePath)
+    }
+    
+    for (var in variableNames) {
+      
+      bcxsubtracted_data<-allDataValue %>% 
+        dplyr::select(DonorID:Stim,{{var}}) %>% 
+        tidyr::pivot_wider(names_from = Stim, values_from = {{var}}) %>% 
+        dplyr::mutate(Covid_WT_sum=ibc(bc(COVID_WT)-bc(DMSO)), Covid_BA4_5_sum=ibc(bc(COVID_BA4_5)-bc(DMSO))) %>% 
+        tidyr::pivot_longer(DMSO:Covid_BA4_5_sum, names_to="Stimulant", values_to = {{var}}) %>%
+        dplyr::mutate(across(DonorID:Stimulant,as.factor)) %>%
+        dplyr::filter(Stimulant != "COVID_WT" & Stimulant != "COVID_BA4_5") %>%
+        dplyr::mutate(Stimulant=fct_recode(Stimulant, COVID_WT="Covid_WT_sum", COVID_BA4_5="Covid_BA4_5_sum")) %>% 
+        dplyr::mutate(Timepoint=fct_relevel(Timepoint,"VY","V2","V3"))
+      
+      #use lapply to apply the Neg to Zero function to all rows
+      #bcxsubtracted_data[[var]] <- lapply(bcxsubtracted_data[[var]], Neg_to_Zero)
+      #bcxsubtracted_data[[var]] <- unlist(bcxsubtracted_data[[var]]) 
+      filePath <- glue::glue("PREVENT_Boxcox_{var}.csv")
+      bcxsubtracted_data <- arrange(bcxsubtracted_data, Stimulant, Timepoint, DonorID)
+      write.csv(bcxsubtracted_data, filePath)
+      lastCreatedFile(filePath)
+    }
+  })
   
   
   
@@ -100,14 +172,11 @@ server <- function(input, output) {
   # Function to prepare original CSV file for download
   output$download <- downloadHandler(
     filename = function() {
-      # Construct filename based on original filename
-      input$file$name
+      paste(Sys.Date(), "transformed-data.csv", sep = "_") # Provide a meaningful default filename
     },
     content = function(file) {
-      # Ensure file is uploaded
-      req(input$file)
-      # Copy the uploaded CSV file to the download location
-      file.copy(input$file$datapath, file)
+      req(lastCreatedFile())  # Ensure there is a file to download
+      file.copy(lastCreatedFile(), file)  # Copy the last created file to the download location
     }
   )
 }
