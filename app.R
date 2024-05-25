7d91b440b68886abe01def849df4c0f65e19b18f
Jasper <halvorsonjasper11@gmail.com>
2024-05-24 19:37:14 -0700

    Generalization, but without user input to specify

--- app.R
+++ app.R
@@ -80,19 +80,20 @@ server <- function(input, output) {
   
   # ---------------------------- Box-Cox Calculation Start --------------------------------------------
   
-  # Create last set of graphs without any parameters highlighted (variables and data read in above)
-  stimulants<-c("DMSO", "Fluzone", "COVID_WT", "COVID_BA4_5", "Cytostim")
+  # These will be eventually user defined
+  stimulants<-c("Fluzone", "COVID_WT", "COVID_BA4_5", "Cytostim")
+  unstimulated_parameter<-"DMSO"
   
   Neg_to_Zero<-function(x){
     ifelse((x<=0.005), 0.005, x)
   }
   
-  # boxcox function (I added a default lambda value of 0.5)
-  #default lambda value of 0.5
+  # Default lambda value of 0.5
   reactiveLambda <- reactive({
     input$lambda  # This will be a number because of numericInput
   })
   
+  # Box Cox function
   bc <- function(x) {
     l <- reactiveLambda()
     if (l == 0) {
@@ -102,7 +103,7 @@ server <- function(input, output) {
     }
   }
   
-  # inverse boxcox function
+  # Inverse Box Cox function
   ibc <- function(x) {
     l <- reactiveLambda()
     if (l == 0) {
@@ -112,6 +113,8 @@ server <- function(input, output) {
     }
   }
   
+  # Stim is special, this is what we use to do operations on groups
+  grouping_columns <- c("Timepoint", "DonorID")
   # Assuming all_data is defined as before, we create a new reactive expression for the filtered data
   all_data_filtered <- reactive({
     req(all_data_raw())  # Ensure all_data is available
@@ -119,44 +122,136 @@ server <- function(input, output) {
     
     # Now arrange the data
     data %>% 
-      arrange(Stim, Timepoint, DonorID)
+      arrange(across(all_of(grouping_columns))) # Arrange by inputted identifier columns instead
   })
-  
   
   observe({
     req(input$patientData, input$AIMVariables) # Ensure both files are uploaded
     allDataValue <- all_data_filtered() # Get the current value of all_data
     variableNames <- variables()$variable # Assuming variables() returns a dataframe with a column 'variable'
     
-    combinedData <- NULL
+    # Group by matching grouping columns
+    grouped_data <- allDataValue %>%
+      group_by(across(all_of(grouping_columns)))
     
-    for (var in variableNames) {
+    # Function to apply Box-Cox transformation, subtract unstimulated control, and apply inverse Box-Cox transformation
+    transform_and_subtract <- function(df, unstim_var) {
+      unstimulated <- df %>% filter(Stim == unstim_var)
+      if (nrow(unstimulated) == 0) return(df)
       
-      bcxsubtracted_data<-allDataValue %>% 
-        dplyr::select(DonorID:Stim,{{var}}) %>% 
-        tidyr::pivot_wider(names_from = Stim, values_from = {{var}}) %>% 
-        dplyr::mutate(Covid_WT_sum=ibc(bc(COVID_WT)-bc(DMSO)), Covid_BA4_5_sum=ibc(bc(COVID_BA4_5)-bc(DMSO))) %>% 
-        tidyr::pivot_longer(DMSO:Covid_BA4_5_sum, names_to="Stimulant", values_to = {{var}}) %>%
-        dplyr::mutate(across(DonorID:Stimulant,as.factor)) %>%
-        dplyr::filter(Stimulant != "COVID_WT" & Stimulant != "COVID_BA4_5") %>%
-        dplyr::mutate(Stimulant=fct_recode(Stimulant, COVID_WT="Covid_WT_sum", COVID_BA4_5="Covid_BA4_5_sum")) %>% 
-        dplyr::mutate(Timepoint=fct_relevel(Timepoint,"VY","V2","V3"))
+      unstim_values <- unstimulated[variableNames]
       
-      # Remove the COVID_BA4_5.x and COVID_WT.x columns
-      bcxsubtracted_data <- bcxsubtracted_data %>% 
-        dplyr::select(DonorID, Stimulant, Timepoint, {{var}})
-      
-      # First iteration: initialize combinedData with bcxsubtracted_data. Otherwise, merge the new data into combinedData by your unique identifiers
-      if (is.null(combinedData)) {
-        combinedData <- bcxsubtracted_data
-      } else {
-        combinedData <- combinedData %>%
-          dplyr::left_join(bcxsubtracted_data, by = c("DonorID", "Stimulant", "Timepoint"))
-      }
+      df %>%
+        mutate(across(all_of(variableNames), ~ ibc(bc(.x) - bc(unstim_values[[cur_column()]]))))
     }
-    write.csv(combinedData, "PREVENT_Boxcox_Combined.csv")
-    lastCreatedFile("PREVENT_Boxcox_Combined.csv")
+    
+    # Apply the transformations within each group
+    transformed_data <- grouped_data %>%
+      group_modify(~ transform_and_subtract(.x, unstimulated_parameter)) %>%
+      ungroup()
+    
+    # # Function to subtract unstimulated control values
+    # subtract_unstimulated <- function(df, unstim_var) {
+    #   unstimulated <- df %>% filter(Stim == unstim_var)
+    #   if (nrow(unstimulated) == 0) return(df)
+    #   
+    #   unstim_values <- unstimulated[variableNames]
+    #   
+    #   df %>% 
+    #     mutate(across(all_of(variableNames), ~ .x - unstim_values[[cur_column()]]))
+    # }
+    # 
+    # # Apply the subtraction within each group
+    # transformed_data <- grouped_data %>%
+    #   group_modify(~ subtract_unstimulated(.x, unstimulated_parameter)) %>%
+    #   ungroup()
+    
+    
+    temp_file <- tempfile(fileext = ".csv")
+    write.csv(transformed_data, temp_file, row.names = FALSE)
+    # write.csv(grouped_data, temp_file, row.names = FALSE)
+    lastCreatedFile(temp_file)
+    
+    
+    
+    
+    
+    
+    # for (var in variableNames) {
+    #   
+    #   # Process data to subtract the unstimulated value from other stimulants
+    #   processedData <- allDataValue %>%
+    #     group_data() %>%  # Use the defined function for grouping
+    #     mutate(
+    #       ReferenceValue = var[Stimulant == unstimulated_parameter]  # Create a reference value column with unstimulated value
+    #     ) %>%
+    #     mutate(
+    #       AdjustedValue = if_else(Stimulant == unstimulated_parameter, Value, Value - ReferenceValue)  # Adjust values
+    #     ) %>%
+    #     ungroup()  # Ungroup data for further processing if necessary
+    #   
+    #   # Optionally filter out the original value column if only adjusted values are needed
+    #   processedData <- processedData %>%
+    #     select(-Value) %>%
+    #     rename(Value = AdjustedValue)
+    #   
+    #   # Merge processed data into combinedData or simply assign it if combinedData is not used elsewhere
+    #   if (is.null(combinedData)) {
+    #     combinedData <- processedData
+    #   } else {
+    #     combinedData <- bind_rows(combinedData, processedData)
+    #   }
+    # }
+    
+    
+    
+    # for (var in variableNames) {
+    #   # Get the Unstimulated parameter value for the 
+    #   
+    #   for (stimulant in stimulants) {
+    #     cat(var, stimulant, "\n")
+    #   }
+    # }
+    
+    # write.csv(combinedData, "PREVENT_Boxcox_Combined.csv")
+    # lastCreatedFile("PREVENT_Boxcox_Combined.csv")
   })
+  
+  
+  # observe({
+  #   req(input$patientData, input$AIMVariables) # Ensure both files are uploaded
+  #   allDataValue <- all_data_filtered() # Get the current value of all_data
+  #   variableNames <- variables()$variable # Assuming variables() returns a dataframe with a column 'variable'
+  #   
+  #   combinedData <- NULL
+  #   
+  #   for (var in variableNames) {
+  #     
+  #     bcxsubtracted_data<-allDataValue %>% 
+  #       dplyr::select(DonorID:Stim,{{var}}) %>% 
+  #       tidyr::pivot_wider(names_from = Stim, values_from = {{var}}) %>% 
+  #       dplyr::mutate(Covid_WT_sum=ibc(bc(COVID_WT)-bc(DMSO)), Covid_BA4_5_sum=ibc(bc(COVID_BA4_5)-bc(DMSO))) %>% 
+  #       tidyr::pivot_longer(DMSO:Covid_BA4_5_sum, names_to="Stimulant", values_to = {{var}}) %>%
+  #       dplyr::mutate(across(DonorID:Stimulant,as.factor)) %>%
+  #       dplyr::filter(Stimulant != "COVID_WT" & Stimulant != "COVID_BA4_5") %>%
+  #       dplyr::mutate(Stimulant=fct_recode(Stimulant, COVID_WT="Covid_WT_sum", COVID_BA4_5="Covid_BA4_5_sum")) %>% 
+  #       dplyr::mutate(Timepoint=fct_relevel(Timepoint,"VY","V2","V3"))
+  #     
+  #     # Remove the COVID_BA4_5.x and COVID_WT.x columns
+  #     bcxsubtracted_data <- bcxsubtracted_data %>% 
+  #       dplyr::select(DonorID, Stimulant, Timepoint, {{var}})
+  #     
+  #     # First iteration: initialize combinedData with bcxsubtracted_data. Otherwise, merge the new data into combinedData by your unique identifiers
+  #     if (is.null(combinedData)) {
+  #       combinedData <- bcxsubtracted_data
+  #     } else {
+  #       combinedData <- combinedData %>%
+  #         dplyr::left_join(bcxsubtracted_data, by = c("DonorID", "Stimulant", "Timepoint"))
+  #     }
+  #   }
+  #   write.csv(combinedData, "PREVENT_Boxcox_Combined.csv")
+  #   lastCreatedFile("PREVENT_Boxcox_Combined.csv")
+  # })
   
   
   
@@ -177,3 +272,5 @@ server <- function(input, output) {
 
 # Create Shiny app object
 shinyApp(ui = ui, server = server)
+
+
